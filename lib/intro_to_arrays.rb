bb213858c54f13aff4b7142841b411f2d2cba3f2
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-19 17:10:13 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,5 +3,30 @@ def instantiate_new_array
 end
 
 def array_with_two_elements(x1,x2)
-  return [x1,x2]
+  array = [x1,x2]
+  return array
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first  
+end
+
+def last_element_with_array_methods(array)
+  return array.last  
+end
+
+def length_of_array(array)
+  return array.length
 end
